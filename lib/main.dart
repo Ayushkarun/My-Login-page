5e316eec2eadc887c71c7a76bbc6f35c38d35c54
Ayush Karun <ayushkarun2580@gmail.com>
2025-02-27 16:55:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import './login/loginp.dart';
 void main() {
   runApp(const Myapp());
 }
+
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
